f2e8a2e855568f6d8521dcca7c1bcc10a3f1f6ef
Kaleab <Kaleaby70@gmail.com>
2025-11-27 16:05:41 +0300

    js started

--- main/TP.js
+++ main/TP.js
@@ -3,3 +3,10 @@ const sidebar = document.getElementById('mySidebar');
 // --- Sidebar ---
 function w3_open() { if (sidebar) sidebar.style.display = 'block'; }
 function w3_close() { if (sidebar) sidebar.style.display = 'none'; }
+
+
+// --- Tasks Completed ---
+const overallFill = document.getElementById('overallFill');
+const weeklyFill = document.getElementById('weeklyFill');
+const monthlyFill = document.getElementById('monthlyFill');
+
