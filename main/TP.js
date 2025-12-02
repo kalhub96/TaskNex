52cfc541a2dafbfd7cdddf48ff9f258c3c03fb44
Kaleab <Kaleaby70@gmail.com>
2025-12-02 13:20:24 +0300

    server

--- main/TP.js
+++ main/TP.js
@@ -10,3 +10,13 @@ const overallFill = document.getElementById('overallFill');
 const weeklyFill = document.getElementById('weeklyFill');
 const monthlyFill = document.getElementById('monthlyFill');
 
+async function loadProgress() {
+    const res = await fetch("/get-progress");
+    const data = await res.json ();
+
+    overallFill.style.width = (data.overall ?? 0) +"%";
+    weeklyFill.style.width = (data.Weekly ?? 0) +"%";
+    monthlyFill.style.width = (data.monthly ?? 0) +"%";
+}
+
+window.onload = loadProgress;
